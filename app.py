9c2362a9a5fb9fe7aa4340fc017c8c4b55f64c53
SDUGAN92 <sharonivdugan@gmail.com>
2025-06-04 16:06:41 -0500

    change

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ df = pd.read_csv('./vehicles_us_cleaned.csv')
 # Show data in the app
 st.write(df)
 
-# histogram of the types of vehicles by manufacturer
+# types of vehicles by manufacturer
 st.subheader('Histogram of the types of vehicles by manufacturer')
 fig = px.histogram(df, x='manufacturer', color='type')
 # plot the histogram
 st.plotly_chart(fig)
 
-# histogram of price distribution between manufacturers
+# price distribution between manufacturers
 st.subheader('Histogram of price distribution between manufacturers')
 # drop down menu for selecting the manufacturer 1 and 2 
 # index 1 and 2 are used to set default values for the drop down menu
